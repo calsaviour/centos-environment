ab317df66cf6357e583671ac7c91c8ef7cbfc763
Low Yan Ming <Calvin.Low@comptel.com>
2016-08-17 16:50:33 +0800

    add installation of pip and epel-release

--- install-python-devel.sh
+++ install-python-devel.sh
@@ -1,11 +1,28 @@
 #!/bin/sh
 echo "Check if python devel tools is installed"
-yum list installed python-devel*
 
+yum list installed python-devel*
 if [ "$?" = "0" ]; then
 	echo "There's already an installation of python devel tools"
-	exit 0
 else
 	echo "Start installation of python devel tools"
 	sudo yum install -y python-devel
+fi
+
+
+yum list installed | grep epel-release.noarch
+if [ "$?" = "0" ]; then
+	echo "There's already an installation of epel-release"
+else
+	echo "Start installation of python devel tools"
+	sudo yum install -y epel-release
+fi
+
+
+pip --version > /dev/null 2>&1
+if [ "$?" = "0" ]; then
+	echo "There's already an installation of pip"
+else
+	echo "Start installation of pip"
+	sudo yum install -y python pip
 fi
